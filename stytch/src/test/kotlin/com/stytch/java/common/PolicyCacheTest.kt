22e418db4c93e73238d31c031f56eac1843a0832
Evelyn Taylor-McGregor <etaylormcgregor@stytch.com>
2025-02-14 08:56:03 -0600

    import

--- stytch/src/test/kotlin/com/stytch/java/common/PolicyCacheTest.kt
+++ stytch/src/test/kotlin/com/stytch/java/common/PolicyCacheTest.kt
@@ -7,6 +7,7 @@ import com.stytch.java.b2b.models.rbac.PolicyResponse
 import com.stytch.java.b2b.models.rbac.PolicyRole
 import com.stytch.java.b2b.models.rbac.PolicyRolePermission
 import com.stytch.java.b2b.models.rbac.PolicyScope
+import com.stytch.java.b2b.models.rbac.PolicyScopePermission
 import com.stytch.java.b2b.models.sessions.AuthorizationCheck
 import io.mockk.every
 import io.mockk.mockk
