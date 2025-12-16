bb11d8041bbc75406215357bdb32a1a54a2a3d27
Jordan Haven <117691317+jhaven-stytch@users.noreply.github.com>
2025-12-16 12:27:49 -0500

    Fix vulns (#137)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "stytch-java"
 include("stytch", "workbench")
+
+plugins {
+    id("com.gradle.develocity") version ("3.17.5")
+}
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
